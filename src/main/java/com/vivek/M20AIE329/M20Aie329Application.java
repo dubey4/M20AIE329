693a9e8d5351756cef4625c475bfd257e425ebed
unknown <vivek.dubeydeveloper@gmail.com>
2021-09-01 18:22:06 +0530

    first commit

---
+++ src/main/java/com/vivek/M20AIE329/M20Aie329Application.java
@@ -0,0 +1,13 @@
+package com.vivek.M20AIE329;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M20Aie329Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M20Aie329Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/M20AIE329/M20Aie329ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.M20AIE329;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M20Aie329ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
